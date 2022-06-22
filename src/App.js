15a41a6dcbd210bbdc5494dec3d88b10db375f94
hagus99 <hagus99@kakao.com>
2022-06-22 23:18:19 +0900

    feat:restAPI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import axios from "axios";
+import React, { useState, useLayoutEffect } from "react";
 
 function App() {
+  const [applicantData, setapplicantData] = useState();
+  const [loading, setLoading] = useState(false);
+  useLayoutEffect(() => {
+    readApplicantData();
+  }, []);
+
+  const readApplicantData = async (programID, userID) => {
+    const response = await axios.get("http://localhost:8000/api/gains/");
+    console.log(response.data[0]);
+    setapplicantData(response.data[0]);
+    setLoading(true);
+  };
+
   return (
     <div className="App">
       <h2>Hi NoPainNo</h2>
+      {loading?applicantData.title:""}
     </div>
-  );
+  )
 }
 
 export default App;
