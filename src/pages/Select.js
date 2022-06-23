3f7c594267e925357d428b0428f7a77538ed43ed
unknown <choibh6258@hanyang.ac.kr>
2022-06-24 03:54:32 +0900

    feat(implement isSelect)

--- src/pages/Select.js
+++ src/pages/Select.js
@@ -11,33 +11,38 @@ import img6 from '../assets/img/character/589.png';
 import img7 from '../assets/img/character/957.png';
 import img8 from '../assets/img/character/972.png';
 import img9 from '../assets/img/character/5088.png';
-import { useState } from 'react';
+import { useState,useRef } from 'react';
+import styled from 'styled-components';
+
 
 
 export default function Select(){
     const [imageSrc, setImageSrc] = useState('');
-    const [selected,setSelected] = useState();
+    const [selected1,setSelected1] = useState(false);
+    const [selected2,setSelected2] = useState(false);
+    const [selected3,setSelected3] = useState(false);
+    const [selected4,setSelected4] = useState(false);
+    const [selected5,setSelected5] = useState(false);
+    const [selected6,setSelected6] = useState(false);
+    const [selected7,setSelected7] = useState(false);
+    const [selected8,setSelected8] = useState(false);
+    const [selected9,setSelected9] = useState(false);
+    const [isOnly,setIsOnly] = useState(true);
 
-    
-    const clickFilter = (e) =>{
-        console.log(e.alt);
-        setSelected(e.alt);
-    }
-    
     return (
         <div className='Select'>
             <div className='sampleImg'>
                 <img className='gainImg' src={gainImg}/>
                 <div className='imgArray'>
-                    <div className="borderBox" onClick={(e) => clickFilter(e.target)}><img alt='1' className='rec' src={img1}/></div>
-                    <div className="borderBox" onClick={(e) => clickFilter(e.target)}><img alt='2' className='rec' src={img2}/></div>
-                    <div className="borderBox" onClick={(e) => clickFilter(e.target)}><img alt='3' className='rec' src={img3}/></div>
-                    <div className="borderBox" onClick={(e) => clickFilter(e.target)}><img alt='4' className='rec' src={img4}/></div>
-                    <div className="borderBox" onClick={(e) => clickFilter(e.target)}><img alt='5' className='rec' src={img5}/></div>
-                    <div className="borderBox" onClick={(e) => clickFilter(e.target)}><img alt='6' className='rec' src={img6}/></div>
-                    <div className="borderBox" onClick={(e) => clickFilter(e.target)}><img alt='7' className='rec' src={img7}/></div>
-                    <div className="borderBox" onClick={(e) => clickFilter(e.target)}><img alt='8' className='rec' src={img8}/></div>
-                    <div className="borderBox" onClick={(e) => clickFilter(e.target)}><img alt='9' className='rec' src={img9}/></div>
+                    <div className={selected1?'selected':'borderBox'} clicked={selected1} onClick={() => setSelected1(!selected1)}><img alt='1' className='rec' src={img1}/></div>
+                    <div className={selected2?'selected':'borderBox'} clicked={selected2} onClick={() => setSelected2(!selected2)}><img alt='2' className='rec' src={img2}/></div>
+                    <div className={selected3?'selected':'borderBox'} clicked={selected3} onClick={() => setSelected3(!selected3)}><img alt='3' className='rec' src={img3}/></div>
+                    <div className={selected4?'selected':'borderBox'} clicked={selected4} onClick={() => setSelected4(!selected4)}><img alt='4' className='rec' src={img4}/></div>
+                    <div className={selected5?'selected':'borderBox'} clicked={selected5} onClick={() => setSelected5(!selected5)}><img alt='5' className='rec' src={img5}/></div>
+                    <div className={selected6?'selected':'borderBox'} clicked={selected6} onClick={() => setSelected6(!selected6)}><img alt='6' className='rec' src={img6}/></div>
+                    <div className={selected7?'selected':'borderBox'} clicked={selected7} onClick={() => setSelected7(!selected7)}><img alt='7' className='rec' src={img7}/></div>
+                    <div className={selected8?'selected':'borderBox'} clicked={selected8} onClick={() => setSelected8(!selected8)}><img alt='8' className='rec' src={img8}/></div>
+                    <div className={selected9?'selected':'borderBox'} clicked={selected9} onClick={() => setSelected9(!selected9)}><img alt='9' className='rec' src={img9}/></div>
                 </div>
                 <div className='instruction'>
                     <p>원하는 필터를 선택해주세요.</p>
